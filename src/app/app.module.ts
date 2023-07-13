a3ede12a01336b976edfc61cf2b164f2aa1ebeb1
Victor Candido <victorjrcandido@gmail.com>
2023-07-12 21:32:00 -0300

    rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { CriarPensamentoComponent } from './components/pensamentos/criar-pensamento/criar-pensamento.component';
 import { FormsModule } from '@angular/forms';
+import { ListarPensamentoComponent } from './components/pensamentos/listar-pensamento/listar-pensamento.component';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { FormsModule } from '@angular/forms';
     HeaderComponent,
     FooterComponent,
     CriarPensamentoComponent,
+    ListarPensamentoComponent,
   ],
   imports: [
     BrowserModule,
